7d92b0cd96ea6ea1728358a99c426547fde6b26d
hoss-mks <hoss-mks@github.com>
2022-02-26 14:03:34 +0100

    Font
    add Font poppins

--- src/screens/Acceuil.tsx
+++ src/screens/Acceuil.tsx
@@ -1,16 +1,54 @@
+import AppLoading from 'expo-app-loading';
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
+import * as Font from 'expo-font';
 
 type Props = {}
 
-const Acceuil = (props: Props) => {
-  return (
-    <View>
-      <Text>Acceuil</Text>
-    </View>
-  )
-}
+export default function Acceuil(props: Props){
+  const getFonts = () =>Font.loadAsync({
+    'Poppins-Bold': require('../../assets/fonts/Poppins-Bold.otf'),
+  });
+    const [fontsloaded, setFontsLoaded] = useState(false);
+  if (fontsloaded) {
+        return(
+            <View style={styles.container}>
+                <View style={styles.carrouselContainer}>
+                    <Text style={styles.carrouselTitle} >Catégories</Text>
+                </View>
+                <View style={styles.carrouselContainer}>
+                    <Text style={styles.carrouselTitle}>Catégories</Text>
+                </View>
+            </View>
+        )
+    } else {
+        return (
+        <AppLoading
+            startAsync={getFonts}
+            onFinish={() => {
+            setFontsLoaded(true);
+            }}
+            onError={console.warn}
+        />
+        );
+    } 
+};
 
-export default Acceuil
-
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white'
+    },
+    carrouselContainer:{
+        flex: 1,
+        marginLeft:15
+    },
+    carrouselTitle:{
+        fontFamily:'Poppins-Bold',
+        textTransform: 'uppercase',
+        fontSize:25,
+        fontWeight:'bold',
+        textAlign:'center',
+        marginBottom:16
+    },
+});
