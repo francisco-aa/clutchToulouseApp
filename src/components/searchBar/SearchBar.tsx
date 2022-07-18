2780126a17f400eb98a37a659e0d586d8566f062
atl31 <pierre_prive@protonmail.com>
2022-07-18 09:10:20 +0100

    fix some types and change date picker mode

--- src/components/searchBar/SearchBar.tsx
+++ src/components/searchBar/SearchBar.tsx
@@ -1,12 +1,10 @@
-import React, {FC, useEffect, useState} from "react";
+import React, {FC} from "react";
 import {SearchBarStyle, SearchBarContainer} from "./searchBar.style";
 import { FontAwesome } from '@expo/vector-icons';
-import {format, isDate} from "date-fns";
-import fr from "date-fns/locale/fr";
 
 type TSearchBar = {
     placeholder: string,
-    iconRight: {name: string, color: string},
+    iconRight: {name: keyof typeof FontAwesome.glyphMap, color: string},
     onSearch: (status: boolean) => any,
     onChange: (value: string) => any,
     currentSearch: string,
