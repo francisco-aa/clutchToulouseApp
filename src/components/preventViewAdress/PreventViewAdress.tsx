c00f8a392a080667e240ab1f77e1b41b3d537e02
fscript98 <francisco.agudo-antonio@agileo-solutions.com>
2022-07-25 16:07:57 +0200

    event details map marker update

--- src/components/preventViewAdress/PreventViewAdress.tsx
+++ src/components/preventViewAdress/PreventViewAdress.tsx
@@ -1,6 +1,8 @@
 import { LatLng, MapViewProps, Marker } from 'react-native-maps'
-import { FC, useEffect, useRef } from 'react'
+import React, { FC, useEffect, useRef } from 'react'
 import { Map } from './preventView.style'
+import { Image } from 'react-native'
+import { markerStyle } from '../map/mapView.style'
 
 type TPreventViewAdress = {
     coordinate: LatLng
@@ -13,7 +15,16 @@ const PreventViewAdress: FC<TPreventViewAdress> = ({ coordinate }) => {
   }, [coordinate])
   return (
     <Map ref={mapRef}>
-      <Marker coordinate={coordinate}/>
+      <Marker coordinate={{
+        latitude: coordinate.latitude,
+        longitude: coordinate.longitude
+      }}>
+        <Image
+          style={markerStyle.markerImage}
+          resizeMode={'contain'}
+          source={require('../../../assets/images/markers/Fant_violet.png')}
+        />
+      </Marker>
     </Map>
   )
 }
